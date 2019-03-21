27afecb99b718e31511bbe733abea45648e08540
neurons4me <adammcdaniel1@gmail.com>
2019-03-20 20:05:14 -0700

    Added the start of a Door model. Modified the Room model to coorespond. Edited some comments.

---
+++ Door.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Net;
+using System.Collections.Generic;
+
+namespace TextAdventure
+{
+
+public class Door
+{
+
+public Room higherNeighbor {get; set;}
+public Room lowerNeighbor {get; set;}
+
+public bool isLocked {get; set;}
+
+public bool isHidden {get; set;}
+
+}
+
+
+}
+
