1be5d077fbe0f2cef9a4188000968ed7e7667654
neurons4me <adammcdaniel1@gmail.com>
2019-03-20 20:07:00 -0700

    Door.cs changes were not saved prior to last commit. Now applied.

--- Room.cs
+++ Room.cs
@@ -11,6 +11,10 @@ public Room northNeighbor {get;set;}
 public Room eastNeighbor {get;set;}
 public Room southNeighbor {get;set;}
 public Room westNeighbor {get;set;}
+public Door northDoor {get; set;}
+public Door eastDoor {get; set;}
+public Door southDoor {get; set;}
+public Door westDoor {get; set;}
 public List<Item> roomInventory { get; set; } = new List<Item>();
 public bool isLocked {get;set;}
 public bool isHidden {get;set;}
